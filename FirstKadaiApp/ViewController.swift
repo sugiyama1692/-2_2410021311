71b78abd0f4c0c1c726d0275e77ea7474412b726
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-02 13:01:01 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by mba2408.starlight kyoei.engine on 2024/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
